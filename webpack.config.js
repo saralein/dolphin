ec993cbaa67faa3093d852b87b2d5bca8691c005
saralein <sara.m.trujillo@gmail.com>
2017-06-29 15:12:13 -0500

    Configure webpack.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+
+const config = {
+  entry: './app/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '*']
+  },
+  module: {
+    rules: [{
+      test: /jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      }]
+    }]
+  }
+};
+
+module.exports = config;
