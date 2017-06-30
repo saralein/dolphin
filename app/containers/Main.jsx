a2c8c167433cfb54b9bbad2596116518cd170646
saralein <sara.m.trujillo@gmail.com>
2017-06-30 09:30:06 -0500

    Subscribe Main to redux state and pass props.

--- app/containers/Main.jsx
+++ app/containers/Main.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Nav from '../components/Nav';
 import Field from '../components/Field';
 import Types from '../components/Types';
 
-let showTypes = true;
+import {showTypes} from '../action-creators/types'
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <Nav />
-        <Field />
-        {showTypes && <Types />}
+        <Field toggleModal={this.props.toggleModal} />
+        {this.props.types.showTypes && <Types toggleModal={this.props.toggleModal} categories={this.props.types.categories} />}
       </div>
     )
   }
 }
 
-export default Main;
+const mapStateToProps = (state) =>{
+  return {
+    types: state.types
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleModal (){
+      dispatch(showTypes())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
