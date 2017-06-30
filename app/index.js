1d70f088238ea952899555a077c3d955a5be1e0b
saralein <sara.m.trujillo@gmail.com>
2017-06-30 09:30:36 -0500

    Set up store in render method.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 
+import store from './store'
 import Main from './containers/Main';
 
 ReactDOM.render(
-  <Main />,
+  <Provider store={store}>
+    <Main />
+  </Provider>,
   document.getElementById('main')
 )
